c985eac05b98b9c4fbbaad2c3622a7101acd957a
Govind Menon <govindmenon1995@yahoo.co.in>
2020-08-05 20:21:56 +0530

    completed matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,44 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let startrow = 0;
+    let startcol = 0;
+    let endrow = n - 1;
+    let endcol = n - 1;
+
+    const resultMatrix = [];
+
+    for(let i=0;i<n;i++){
+        resultMatrix[i] = [];
+    }
+
+    let counter = 1;
+
+    while(startrow <= endrow && startcol <= endcol){
+        for(let i=startcol;i<=endcol;i++){
+            resultMatrix[startrow][i] = counter;
+            counter++;
+        }
+        startrow++;
+        for(let i=startrow;i<=endrow;i++){
+            resultMatrix[i][endcol] = counter;
+            counter++;
+        }
+        endcol--;
+        for(let i=endcol;i>=startcol;i--){
+            resultMatrix[endrow][i] = counter;
+            counter++;
+        }
+        endrow--;
+        for(let i=endrow;i>=startrow;i--){
+            resultMatrix[i][startcol] = counter;
+            counter++;
+        }
+        startcol++;
+    }
+
+    return resultMatrix;
+}
 
 module.exports = matrix;
