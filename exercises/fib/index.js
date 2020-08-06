cb57fc32d544c7ee64aafce8d1c9286c65b37a6d
Govind Menon <govindmenon1995@yahoo.co.in>
2020-08-06 19:33:19 +0530

    Completed fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,48 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Solution #1
+/*
+function fib(n) {
+    let result = [0,1]; 
+
+    for(let i=2;i<=n;i++){
+        result[i] = result[i-2] + result[i-1];
+    }
+
+    return result[n];
+}
+*/
+
+//Solution #2
+/*
+function fib(n) {
+    if(n<=1){
+        return n;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+*/
+
+//Solution #3
+
+function fib(n, map = {0:0,1:1}) {
+    if(map[n]){
+        return map[n];
+    }
+
+    if(map[n-2] === undefined){
+        map[n-2] = fib(n-2, map);
+    }
+
+    if(map[n-1] === undefined){
+        map[n-1] = fib(n-1, map);
+    }
+
+    map[n] = map[n-2] + map[n-1];
+    return map[n];
+}
+
 
 module.exports = fib;
